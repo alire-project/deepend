c196d627cc01e28f332f91cb2f690cc60dc0ab75
Brad Moore <billymeister@users.sourceforge.net>
2012-07-03 21:57:58 -0600

    Updated to Ada2012 release

--- 2005/dynamic_pools.adb
+++ 2005/dynamic_pools.adb
@@ -1,30 +1,30 @@
 ------------------------------------------------------------------------------
---                                                                          --
---                   Deepend - Dynamic Pools for Ada 2005                   --
---                                                                          --
---                        D Y N A M I C   P O O L S                         --
---                                                                          --
---                                B o d y                                   --
---                                                                          --
---                  Copyright (C) 2011, Bradley J. Moore                    --
---                                                                          --
---  Deepend is free software;  you can  redistribute it  and/or modify it   --
---  under  terms of the  GNU General Public License  as  published  by the  --
---  Free Software  Foundation;  either version 2,  or (at your option) any  --
---  later  version.  Paraffin is  distributed in the hope that it  will be  --
---  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of  --
---  MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR PURPOSE.  See the GNU  --
---  General Public License for  more details.  You should have  received a  --
---  copy of the GNU General Public License distributed with Deepend;  see  --
---  file  COPYING.  If  not,  write  to  the  Free  Software  Foundation,   --
---  51 Franklin  Street,  Fifth  Floor, Boston, MA 02110-1301, USA.         --
---                                                                          --
---  As a  special exception, if other files  instantiate generics from      --
---  this unit,  or you link this  unit with other files  to produce an      --
---  executable,  this unit  does  not by  itself  cause the  resulting      --
---  executable to be covered by  the GNU General Public License.  This      --
---  exception does  not however invalidate  any other reasons  why the      --
---  executable file might be covered by the GNU Public License.             --
+--
+--              Deepend - Dynamic Pools for Ada 2005 and Ada 2012
+--
+--                        D Y N A M I C   P O O L S
+--
+--                                B o d y
+--
+--                  Copyright (C) 2011, Bradley J. Moore
+--
+--  Deepend is free software;  you can  redistribute it  and/or modify it
+--  under  terms of the  GNU General Public License  as  published  by the
+--  Free Software  Foundation;  either version 2,  or (at your option) any
+--  later  version.  Paraffin is  distributed in the hope that it  will be
+--  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR PURPOSE.  See the GNU
+--  General Public License for  more details.  You should have  received a
+--  copy of the GNU General Public License distributed with Deepend;  see
+--  file  COPYING.  If  not,  write  to  the  Free  Software  Foundation,
+--  51 Franklin  Street,  Fifth  Floor, Boston, MA 02110-1301, USA.
+--
+--  As a  special exception, if other files  instantiate generics from
+--  this unit,  or you link this  unit with other files  to produce an
+--  executable,  this unit  does  not by  itself  cause the  resulting
+--  executable to be covered by  the GNU General Public License.  This
+--  exception does  not however invalidate  any other reasons  why the
+--  executable file might be covered by the GNU Public License.
 ------------------------------------------------------------------------------
 
 with Ada.Unchecked_Deallocation;
@@ -76,7 +76,7 @@ package body Dynamic_Pools is
 
       procedure Deallocate_All
       is
-         procedure Deallocate_Pools (Position : Subpool_Vector.Cursor) is
+         procedure Deallocate_Subpools (Position : Subpool_Vector.Cursor) is
             Subpool : Dynamic_Subpool_Access :=
               Subpool_Vector.Element (Position);
          begin
@@ -90,9 +90,9 @@ package body Dynamic_Pools is
             Free_Storage_Array (Subpool.Active);
 
             Free_Subpool (Subpool);
-         end Deallocate_Pools;
+         end Deallocate_Subpools;
       begin
-         Subpools.Iterate (Deallocate_Pools'Access);
+         Subpools.Iterate (Deallocate_Subpools'Access);
       end Deallocate_All;
 
       --------------------------------------------------------------
@@ -102,7 +102,9 @@ package body Dynamic_Pools is
       begin
          Position := Subpools.Find (Subpool);
 
+         pragma Warnings (Off, "*Position*modified*but*never referenced*");
          Subpools.Delete (Position);
+         pragma Warnings (On, "*Position*modified*but*never referenced*");
       end Delete;
 
    end Subpool_Set;
@@ -135,8 +137,7 @@ package body Dynamic_Pools is
       Storage_Address : out Address;
       Size_In_Storage_Elements : Storage_Elements.Storage_Count;
       Alignment : Storage_Elements.Storage_Count;
-      Subpool : --  not null
-      Subpool_Handle)
+      Subpool : not null Subpool_Handle)
    is
       pragma Unreferenced (Alignment, Pool);
       use type System.Storage_Elements.Storage_Offset;
@@ -182,11 +183,8 @@ package body Dynamic_Pools is
    begin
 
       --  pragma Assert (Allocation_Type_Access'Storage_Size /= 0);
-      pragma Compile_Time_Warning
-        (Ada2012_Warnings,
-         "In Ada 2012, this should be a precondition");
 
-      Pool_Of_Subpool (Subpool).Allocate_From_Subpool
+      Storage_Pools.Subpools.Pool_Of_Subpool (Subpool).Allocate_From_Subpool
         (Storage_Address => Location,
          Size_In_Storage_Elements =>
            Allocation_Type'Max_Size_In_Storage_Elements,
@@ -221,22 +219,21 @@ package body Dynamic_Pools is
    is
       New_Pool : constant Dynamic_Subpool_Access
         := new Dynamic_Subpool'
-          (Root_Subpool with
+          (Storage_Pools.Subpools.Root_Subpool with
            Block_Size => Block_Size,
            Used_List => <>,
            Free_List => <>,
            Active => new System.Storage_Elements.Storage_Array
              (1 .. Block_Size),
            Next_Allocation => 1,
-           Owner => Ada.Task_Identification.Current_Task,
-           Deallocate_Storage => True);
+           Owner => Ada.Task_Identification.Current_Task);
 
       Result : constant Subpool_Handle := New_Pool.all'Unchecked_Access;
    begin
 
       Pool.Subpools.Add (New_Pool);
 
-      Set_Pool_Of_Subpool
+      Storage_Pools.Subpools.Set_Pool_Of_Subpool
         (Subpool => Result,
          To => Pool.all);
 
@@ -266,39 +263,37 @@ package body Dynamic_Pools is
    is
       The_Subpool : Dynamic_Subpool_Access
         := Dynamic_Subpool (Subpool.all)'Access;
+
+      use type Storage_Pools.Subpools.Subpool_Handle;
    begin
 
-      if The_Subpool.Deallocate_Storage then
+      --  Only removes the access value from the Subpools container
+      --  Does not actually delete the object which we still have a
+      --  reference to above
+      Pool.Subpools.Delete (The_Subpool);
 
-         Pool.Subpools.Delete (The_Subpool);
+      The_Subpool.Used_List.Iterate
+        (Process => Free_Storage_Element'Access);
 
-         The_Subpool.Used_List.Iterate
-           (Process => Free_Storage_Element'Access);
+      The_Subpool.Used_List.Clear;
 
-         The_Subpool.Used_List.Clear;
+      The_Subpool.Free_List.Iterate
+        (Process => Free_Storage_Element'Access);
 
-         The_Subpool.Free_List.Iterate
-           (Process => Free_Storage_Element'Access);
+      The_Subpool.Free_List.Clear;
+      Free_Storage_Array (The_Subpool.Active);
 
-         The_Subpool.Free_List.Clear;
-         Free_Storage_Array (The_Subpool.Active);
-
-         Free_Subpool (The_Subpool);
-      else
-         --  If we had access to the Ada finalization functionality, we would
-         --  have stored called that functionality here to finalize objects
-         --  needing finalization.
-         --  Since we don't have access to this functionality in Ada 2005,
-         --  all we can do is call Clear.
-
-         The_Subpool.Free_List.Append
-           (New_Item => The_Subpool.Used_List);
-
-         The_Subpool.Used_List.Clear;
-         The_Subpool.Next_Allocation := 1;
-         The_Subpool.Deallocate_Storage := False;
-
+      --  Handle case when deallocating the default pool
+      --  Should only occur if client attempts to obtain the default
+      --  subpool, then calls Unchecked_Deallocate_Subpool on that object
+      if Pool.Default_Subpool /= null and then
+        The_Subpool = Dynamic_Subpool
+          (Pool.Default_Subpool.all)'Access
+      then
+         Pool.Default_Subpool := null;
       end if;
+
+      Free_Subpool (The_Subpool);
 
    end Deallocate_Subpool;
 
@@ -327,7 +322,9 @@ package body Dynamic_Pools is
       procedure Finalize (Scoped_Subpool : in out Scoped_Subpool_Handle) is
          Subpool : Subpool_Handle := Scoped_Subpool.Handle;
       begin
+         pragma Warnings (Off, "*Subpool*modified*but*never referenced*");
          Unchecked_Deallocate_Subpool (Subpool);
+         pragma Warnings (On, "*Subpool*modified*but*never referenced*");
       end Finalize;
    end Scoped_Subpools;
 
@@ -370,11 +367,8 @@ package body Dynamic_Pools is
    begin
 
       --  pragma Assert (Allocation_Type_Access'Storage_Size /= 0);
-      pragma Compile_Time_Warning
-        (Ada2012_Warnings,
-         "In Ada 2012, this should be a precondition");
 
-      Pool_Of_Subpool (Subpool).Allocate_From_Subpool
+      Storage_Pools.Subpools.Pool_Of_Subpool (Subpool).Allocate_From_Subpool
         (Storage_Address => Location,
          Size_In_Storage_Elements =>
            Qualified_Expression'Size /
@@ -453,48 +447,19 @@ package body Dynamic_Pools is
 
    --------------------------------------------------------------
 
-   procedure Unchecked_Deallocate_Objects
-     (Subpool : Subpool_Handle) is
-   --  Currently only the vendor could supply such a routine. Ada 2012
-   --  will expose this functionality, but for now we have to say that
-   --  it is erroneous to allocate objects needing finalization such as tasks,
-   --  protected objects and type derived types defined in Ada.Finalization
-   --  from a dynamic pool.
-
-      Copy : Subpool_Handle := Subpool;
-
-   begin
-
-      --  Set the flag that prevents the subpool storage from being freed. Only
-      --  the objects will be freed.
-      Dynamic_Subpool
-        (Subpool.all).Deallocate_Storage := False;
-
-      --  Since Ada.Unchecked_Deallocate_Subpool doesn't exist currently,
-      --  dispatch to Deallocate_Subpool directly for now.
-      Pool_Of_Subpool (Subpool).Deallocate_Subpool (Copy);
-
-      --  As per AI05-0111-3
-      --  Ada.Unchecked_Deallocate_Subpool (Copy);
-      --  In this case, Copy will not be set to null after this call
-   end Unchecked_Deallocate_Objects;
-
-   --------------------------------------------------------------
-
    procedure Unchecked_Deallocate_Subpool
-     (Subpool : in out Subpool_Handle) is
+     (Subpool : in out Subpool_Handle)
+   is
+      use type Storage_Pools.Subpools.Subpool_Handle;
    begin
       if Subpool = null then
          return;
       end if;
 
-      --  Set the flag that prevents the subpool storage from being freed. Only
-      --  the objects will be freed.
-      Dynamic_Subpool (Subpool.all).Deallocate_Storage := True;
-
       --  Since Ada.Unchecked_Deallocate_Subpool doesn't exist currently,
       --  dispatch to Deallocate_Subpool directly for now.
-      Pool_Of_Subpool (Subpool).Deallocate_Subpool (Subpool);
+      Storage_Pools.Subpools.Pool_Of_Subpool
+        (Subpool).Deallocate_Subpool (Subpool);
 
       --  As per AI05-0111-3
       --  Ada.Unchecked_Deallocate_Subpool (Subpool);
