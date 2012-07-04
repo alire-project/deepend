fc7c3c1e4815765dad3da2d2b69f32dfc8b662da
Brad Moore <billymeister@users.sourceforge.net>
2012-07-03 21:58:18 -0600

    Updated to Ada2012 release

--- 2005/dynamic_pools.ads
+++ 2005/dynamic_pools.ads
@@ -1,40 +1,49 @@
 ------------------------------------------------------------------------------
---                                                                          --
---                   Deepend - Dynamic Pools for Ada 2005                   --
---                                                                          --
---                        D Y N A M I C   P O O L S                         --
---                                                                          --
---                                S p e c                                   --
---                                                                          --
---                  Copyright (C) 2011, Bradley J. Moore                    --
---                                                                          --
---  Deepend is free software;  you can  redistribute it  and/or modify it  --
---  under  terms of the  GNU General Public License  as  published  by the  --
---  Free Software  Foundation;  either version 2,  or (at your option) any  --
---  later  version.  Paraffin is  distributed in the hope that it  will be  --
---  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of  --
---  MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR PURPOSE.  See the GNU  --
---  General Public License for  more details.  You should have  received a  --
---  copy of the GNU General Public License distributed with Deepend;  see   --
---  file  COPYING.  If  not,  write  to  the  Free  Software  Foundation,   --
---  51 Franklin  Street,  Fifth  Floor, Boston, MA 02110-1301, USA.         --
---                                                                          --
---  As a  special exception, if other files  instantiate generics from      --
---  this unit,  or you link this  unit with other files  to produce an      --
---  executable,  this unit  does  not by  itself  cause the  resulting      --
---  executable to be covered by  the GNU General Public License.  This      --
---  exception does  not however invalidate  any other reasons  why the      --
---  executable file might be covered by the GNU Public License.             --
+--
+--              Deepend - Dynamic Pools for Ada 2005 and Ada 2012
+--
+--                        D Y N A M I C   P O O L S
+--
+--                                S p e c
+--
+--                  Copyright (C) 2011, Bradley J. Moore
+--
+--  Deepend is free software;  you can  redistribute it  and/or modify it
+--  under  terms of the  GNU General Public License  as  published  by the
+--  Free Software  Foundation;  either version 2,  or (at your option) any
+--  later  version.  Paraffin is  distributed in the hope that it  will be
+--  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR PURPOSE.  See the GNU
+--  General Public License for  more details.  You should have  received a
+--  copy of the GNU General Public License distributed with Deepend;  see
+--  file  COPYING.  If  not,  write  to  the  Free  Software  Foundation,
+--  51 Franklin  Street,  Fifth  Floor, Boston, MA 02110-1301, USA.
+--
+--  As a  special exception, if other files  instantiate generics from
+--  this unit,  or you link this  unit with other files  to produce an
+--  executable,  this unit  does  not by  itself  cause the  resulting
+--  executable to be covered by  the GNU General Public License.  This
+--  exception does  not however invalidate  any other reasons  why the
+--  executable file might be covered by the GNU Public License.
 ------------------------------------------------------------------------------
 
---  Deepend provides storage pools written in Ada 2005 that are intended to be
---  compatible as much as possible with the Ada 2012 proposal for creating
---  storage subpools. Once Ada 2012 becomes available, Deepend will be
---  updated to be fully compliant with Ada 2012 Subpools. The only
---  significant incompatible changes to the interface will be involving
---  function calls that have in out parameters instead of access parameters,
---  and likely the elimination of the Allocation and Initialized_Allocation
---  generics, since they will no longer be needed.
+--  Deepend provides storage pools with subpool capabilities for Ada 2005
+--  and Ada 2012. Subpools provide an efficient mechanism to release
+--  storage that is considered to be safer and more efficient than
+--  other strategies such as garbage collection. In Ada 2012, the new
+--  allocation syntax may be used with this pool, to specify the subpool that
+--  will contain the allocated objects.
+--
+--     e.g.
+--          Object := new (subpool_name) Object_Type'(Value);
+--
+--  For Ada 2005, the same effect can be obtained by using the Allocation
+--  and Initialized_Allocation generics provided by this package. These
+--  generics allow allocating non-controlled objects of definite types to a
+--  particular subpool.
+--  In addition, for both Ada 2005 and Ada 2012, the new operator may be used
+--  without specifying a subpool, which results in an object being allocated
+--  to the default subpool for the storage pool.
 --
 --  A Dynamic_Pool allows objects allocated from the pool to be reclaimed
 --  all at once, instead of requiring each object to be individually
@@ -50,7 +59,26 @@
 --  tasks to allocate from subpool owned by another task results in
 --  a Program_Error exception being raised.
 --
-
+--  There is both an Ada 2005 version of the packages and an Ada 2012 version.
+--  The Ada 2005 version was designed to be compatible with the Ada 2012
+--  version as much as possible, although the Ada 2012 version does take
+--  advantage of the new features of the language, including the new subpool
+--  facility provided by System.Storage_Pools.Subpools.
+--
+--  Also, the 2012 version eliminates access parameters in favor of in out
+--  parameters for function calls, and allows defaults for discriminated types.
+--  The Ada 2005 version of the Dynamic_Pools package does not support
+--  allocations of unconstrained types, or objects that need finalization such
+--  as controlled types, or protected types, although there is a Deepend
+--  package, Basic_Dynamic_Pools, that does provide these capabilities in
+--  Ada 2005, though that package does not provide subpool capabilities.
+--
+--  The Ada 2012 Dynamic_Pools package provides both subpool capabilities and
+--  the ability to allocate controlled types and unconstrained types.
+--  Calling Unchecked_Deallocate_Subpool will finalize these objects and
+--  release any other storage associated with the subpool.
+--
+--
 --  Allocation strategy:
 --
 --    Deallocate is not needed or used, and is implemented as a null
@@ -63,29 +91,21 @@
 --    other strategies such as garbage collection, or individual object
 --    reclamation in a more deterministic fashion.
 --
---  ** NOTE: It is erroneous to allocate objects that need finalization
---  eg. (Tasks, or objects of types inherited from types defined in
---       Ada.Finalization)
---  and then Release the storage associated with those objects before
+--  ** NOTE: In the Ada 2005 version of Dynamic_Pools, it is erroneous to
+--    allocate objects that need finalization eg. (Tasks, protected types,
+--    or objects of types inherited from types defined in Ada.Finalization)
+--  and then deallocate the subpool associated with those objects before
 --  they would have otherwise been finalized.
---  Once Ada 2012 becomes available, it will be possible to allocate
---  objects of controlled types needing finalization. Currently Ada 2012
---  is proposing to disallow task allocations from subpools however.
---
---  Also, the interface provided here does not allow allocating unconstrained
---  objects, except by using Ada's new operator. In that case, you cannot
---  allocate from a specific subpool, but each Dynamic_Pool object has a
---  default subpool. Using the new operator allocates from this default
---  subpool. Ada 2012 is proposing to provide new syntax for the new operator
---  that will allow allocating from a specific subpool. This will eliminate
---  the need for the allocation generics in this package, as well as provide
---  a means to allocate unconstrained objects from specific subpools.
+
+--  For Ada 2012, it is only erroneous to allocate task objects or objects
+--  containing task components to a subpool.
 
 with Ada.Task_Identification; use Ada.Task_Identification;
 with Ada.Finalization;
+
 with System.Storage_Elements; use System;
-with Sys.Storage_Pools.Subpools; use Sys.Storage_Pools.Subpools;
-use Sys; use Sys.Storage_Pools;
+with Sys.Storage_Pools.Subpools;
+use Sys;
 
 private with Ada.Containers.Vectors;
 
@@ -93,9 +113,7 @@ package Dynamic_Pools is
    pragma Elaborate_Body;
    --  Needed to ensure that library routines can execute allocators
 
-   Ada2012_Warnings : constant Boolean := False;
-   --  Set to true to generate compiler warnings about changes needed
-   --  for Ada 2012
+   subtype Subpool_Handle is Storage_Pools.Subpools.Subpool_Handle;
 
    package Scoped_Subpools is
       --  Scoped subpools define a controlled object that wraps a subpool
@@ -115,7 +133,6 @@ package Dynamic_Pools is
 
    end Scoped_Subpools;
 
-   subtype Subpool_Handle is Storage_Pools.Subpools.Subpool_Handle;
    subtype Scoped_Subpool_Handle is Scoped_Subpools.Scoped_Subpool_Handle;
 
    Default_Allocation_Block_Size : constant := 16#FFFF#;
@@ -132,9 +149,6 @@ package Dynamic_Pools is
    --  implies that a default subpool is not needed and therefore is not
    --  created. However, in that case, the overriding Create_Subpool call will
    --  use the Default_Allocation_Block_Size value for the Block Size
-   pragma Compile_Time_Warning
-     (Ada_2012_Warnings, "In Ada 2012, " &
-      "use Default_Allocation_Block_Size as default discriminant");
 
    overriding
    function Create_Subpool
@@ -187,19 +201,8 @@ package Dynamic_Pools is
    --  owner to a Null_Task_Id. Another task may obtain ownership of a subpool,
    --  provided that the subpool has no owner.
 
-   procedure Unchecked_Deallocate_Objects
-     (Subpool : Subpool_Handle);
-   --  This call performs unchecked storage deallocation of all objects
-   --  allocated from the subpool. The subpool itself is not destroyed by this
-   --  call. The intent is to allow new objects to be allocated from the
-   --  subpool after this call, which should be more efficient than calling
-   --  Unchecked_Deallocate_Subpool, and then having to call Create_Subpool
-   --  to create a new subpool.
-
    procedure Unchecked_Deallocate_Subpool
      (Subpool : in out Subpool_Handle);
-   pragma Compile_Time_Warning
-     (Ada2012_Warnings, "For Ada 2012, rename procedure to Ada.U_D_S");
    --  This call performs unchecked storage deallocation of all objects
    --  allocated from the subpool, then destroys the subpool, setting
    --  Subpool to null.
@@ -216,8 +219,6 @@ package Dynamic_Pools is
       type Allocation_Type_Access is access Allocation_Type;
    function Allocation
      (Subpool : Subpool_Handle) return Allocation_Type_Access;
-   pragma Compile_Time_Warning
-     (Ada2012_Warnings, "For Ada 2012, eliminate this generic");
    --  This generic routine provides a mechanism to allocate an object of
    --  a definite subtype from a specific subpool.
 
@@ -227,8 +228,6 @@ package Dynamic_Pools is
    function Initialized_Allocation
      (Subpool : Subpool_Handle;
       Qualified_Expression : Allocation_Type) return Allocation_Type_Access;
-   pragma Compile_Time_Warning
-     (Ada2012_Warnings, "For Ada 2012, eliminate this generic");
    --  This generic routine provides a mechanism to allocate an object of
    --  a definite subtype from a specific subpool, and initializing the
    --  new object with a specific value.
@@ -265,22 +264,22 @@ private
    end Subpool_Set;
 
    type Dynamic_Subpool
-     (Block_Size : Storage_Elements.Storage_Count) is new Root_Subpool with
+     (Block_Size : Storage_Elements.Storage_Count) is
+     new Storage_Pools.Subpools.Root_Subpool with
       record
          Used_List : Storage_Vector.Vector;
          Free_List : Storage_Vector.Vector;
          Active : Storage_Array_Access;
          Next_Allocation : System.Storage_Elements.Storage_Offset;
          Owner : Ada.Task_Identification.Task_Id;
-         Deallocate_Storage : Boolean;
       end record;
 
-   type Dynamic_Pool (Default_Block_Size : Storage_Elements.Storage_Count) is
-     new Root_Storage_Pool_With_Subpools with
-      record
-         Default_Subpool : Subpool_Handle;
-         Subpools : Subpool_Set;
-      end record;
+   type Dynamic_Pool (Default_Block_Size : Storage_Elements.Storage_Count)
+     is new Storage_Pools.Subpools.Root_Storage_Pool_With_Subpools
+   with record
+      Default_Subpool : Subpool_Handle;
+      Subpools : Subpool_Set;
+   end record;
 
    overriding
    procedure Allocate
@@ -295,10 +294,7 @@ private
       Storage_Address : out Address;
       Size_In_Storage_Elements : Storage_Elements.Storage_Count;
       Alignment : Storage_Elements.Storage_Count;
-      Subpool : -- not null
-      Subpool_Handle);
-   pragma Compile_Time_Warning
-     (Ada2012_Warnings, "GNAT Ada 2005 compiler bug, can't use not null here");
+      Subpool : not null Subpool_Handle);
 --   pragma Precondition
 --     (Is_Owner (Subpool, Current_Task));
 --   We want Allocate_From_Subpool to be fast. The commented out precondition
@@ -321,7 +317,7 @@ private
    procedure Finalize   (Pool : in out Dynamic_Pool);
 
    pragma Inline
-     (Unchecked_Deallocate_Objects, Allocate, Default_Subpool_for_Pool,
+     (Allocate, Default_Subpool_for_Pool,
       Initialize, Finalize, Is_Owner, Set_Owner);
 
 end Dynamic_Pools;
