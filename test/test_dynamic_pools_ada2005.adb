7aa116abfa956a082e4e961bb8519feb44a06b3f
Brad Moore <billymeister@users.sourceforge.net>
2012-07-03 22:00:59 -0600

    Updated to Ada2012 release

--- test/test_dynamic_pools_ada2005.adb
+++ test/test_dynamic_pools_ada2005.adb
@@ -1,5 +1,7 @@
 with Dynamic_Pools; use Dynamic_Pools;
 with Ada.Text_IO; use Ada.Text_IO;
+with System.Storage_Elements; use System;
+
 procedure Test_Dynamic_Pools_Ada2005
 is
    Pool : aliased Dynamic_Pools.Dynamic_Pool
@@ -7,6 +9,10 @@ is
 
    subtype Id_String is String (1 .. 10);
    type Id_String_Access is access Id_String;
+   for Id_String_Access'Storage_Pool use Pool;
+
+   type String_Access is access String;
+   for String_Access'Storage_Pool use Pool;
 
    type Node_Type is record
       Value : Integer;
@@ -22,6 +28,18 @@ is
      (Node_Type,
       Node_Access);
 
+   type Ordinary_Type is
+      record
+         Value : Integer;
+      end record;
+
+   type O_Access is access Ordinary_Type;
+   for O_Access'Storage_Pool use Pool;
+
+   function New_Ordinary_Type is new Dynamic_Pools.Allocation
+     (Ordinary_Type,
+      O_Access);
+
    function New_String is new Dynamic_Pools.Initialized_Allocation
      (Allocation_Type => Id_String,
       Allocation_Type_Access => Id_String_Access);
@@ -30,22 +48,9 @@ is
    is
       Sub_Pool : constant Dynamic_Pools.Subpool_Handle
         := Dynamic_Pools.Create_Subpool (Pool'Access);
+
       Node : constant Node_Access := New_Node (Sub_Pool);
-      --  Note: Objects of String_Access type are finalized before returning
-      --  However, String_Access type does not need finalization, and
-      --  Unchecked_Deallocation on the String_Access doesn't do anything,
-      --  (See Dynamic_Pool spec).
-      --  We are obtaining Unchecked_Access of the string below, and because
-      --  the storage pool lifetime of a subpool extends to that of its parent
-      --  the anonymous string access should be guaranteed to point to the
-      --  memory for the lifetime of the top level storage pool.
-      --  ie. Is is safe to print out the tree after all recursion has
-      --  completed.
-      --  However, it is understandable that people might feel queezy about
-      --  this. Ada 2012 will disallow this, as the access type will need
-      --  to have the same accessibility level as the Pool object.
-      type String_Access is access String;
-      for String_Access'Storage_Pool use Pool;
+
       Name : constant String_Access :=
         new String'("Depth=" & Natural'Image (Depth));
 
@@ -75,12 +80,45 @@ is
       if List.Next /= null then
          Print (List.Next.all);
       end if;
+
       Put_Line (Integer'Image (List.Value) &
                 ", Name=<" & List.Name.all &
                 ">, Desc=<" & List.Description.all & '>');
    end Print;
 
    List : constant Node_Access := Recurse (10);
+
 begin
+
+   begin
+
+      declare
+         Sub_Pool : Dynamic_Pools.Subpool_Handle
+           := Dynamic_Pools.Create_Subpool (Pool'Access);
+      begin
+
+         for I in 1 .. 10 loop
+            declare
+               Object : constant O_Access
+                 := New_Ordinary_Type (Sub_Pool);
+            begin
+               Put_Line ("Object Value=" & Natural'Image (Object.Value));
+            end;
+         end loop;
+
+         Put_Line ("Bytes Stored=" &
+                     Storage_Elements.Storage_Count'Image (Pool.Storage_Size));
+
+         Put_Line ("Deallocating Subpool...");
+
+         pragma Warnings (Off, "*Sub_Pool* modified*but*never referenced*");
+         Dynamic_Pools.Unchecked_Deallocate_Subpool (Sub_Pool);
+         pragma Warnings (On, "*Sub_Pool* modified*but*never referenced*");
+      end;
+
+      Put_Line ("Bytes Stored=" &
+                  Storage_Elements.Storage_Count'Image (Pool.Storage_Size));
+   end;
+
    Print (List.all);
 end Test_Dynamic_Pools_Ada2005;
