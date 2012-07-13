6071e14c9610e0aa8a0b1e026b65ce21e2d080eb
billymeister <billymeister@users.sourceforge.net>
2012-07-13 06:51:49 -0600

    Restructured folders

--- 95/sys-storage_pools.ads
+++
@@ -1,8 +0,0 @@
---  Dummy package intended to represent the standard System.Storage_Pools
---  package.
---  This makes it easier to make it appear that a child of System.Storage_Pools
---  is being used, since you cant declare your own child packages
---  under System. When Ada 2012 is available, this package will be deleted.
-package Sys.Storage_Pools is
-   pragma Preelaborate;
-end Sys.Storage_Pools;
